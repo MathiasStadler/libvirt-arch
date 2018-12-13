25914bb173ac212d7b4e88d03150bd6a83b40d31
trapapa <stadler-mathias@web.de>
2018-12-13 10:31:55 +0100

    fix pxe

--- virsh-install-arch-pxe-on-kvm.sh
+++ virsh-install-arch-pxe-on-kvm.sh
@@ -1,5 +1,15 @@
-sudo virt-install --name arch-pxe --ram 4096 \
---disk path=/var/lib/libvirt/images/arch-pxe.qcow2,size=20 \
+#!/bin/sh
+
+if [ -z "$1" ] ;
+then
+ echo Specify a virtual-machine name.
+ exit 1
+fi
+
+sudo virt-install \
+--name $1 \
+--ram 4096 \
+--disk path=/var/lib/libvirt/images/$1.img,size=20 \
 --vcpus 2 \
 --os-type linux \
 --os-variant ubuntu16.04 \
