22d7bd68e34dcba5b66dd292671270dd2b11bc98
MathiasStadler <email@mathias-stadler.de>
2018-12-14 11:44:44 +0100

    add dsnmasq_virbr10.conf

--- virsh-install-arch-pxe-on-kvm.sh
+++ virsh-install-arch-pxe-on-kvm.sh
@@ -10,10 +10,10 @@ sudo virt-install \
 --pxe \
 --name $1 \
 --ram 4096 \
---disk path=/var/lib/libvirt/images/$1.img,size=20 \
+--disk path=/var/lib/libvirt/images/$1.img,size=20,bus=virtio \
 --vcpus 2 \
 --os-type linux \
 --os-variant ubuntu16.04 \
---network bridge=virbr10 \
+--network bridge=virbr10,model=virtio \
 --noautoconsole --graphics=vnc \
 
